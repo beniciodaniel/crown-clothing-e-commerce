fbde924cdcc05344c1aefea1167fe08bfeec3091
Benicio <benicio.daniel@gmail.com>
2020-04-12 19:34:11 -0300

    Creates the CustonButton component e imports it into SignIn

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-buttom/custom-button.component';
 
 class SignIn extends React.Component {
 
@@ -54,7 +55,9 @@ class SignIn extends React.Component {
             required 
           />
 
-          <input type="submit" value='Submit Form' />
+          <CustomButton type="submit">
+            Sign In
+          </CustomButton>
         </form>
       </div>
     )
