4362ef6105f3ea1cdf77b3390de56ae7e434213e
Benicio <benicio.daniel@gmail.com>
2020-05-03 12:16:03 -0300

    Styled-Component Part14 FINISHED changing CSS to Styled: SignUp

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,7 +5,9 @@ import CustomButton from '../custom-buttom/custom-button.component';
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss';
+// import './sign-up.styles.scss';
+
+import { SignUpContainer, SignUpTitle } from './sign-up.styles'
 
 class SignUp extends React.Component {
   constructor() {
@@ -63,8 +65,8 @@ class SignUp extends React.Component {
     const { displayName, email, password, confirmPassword } = this.state;
 
     return(
-      <div className="sign-up">
-        <h2 className='title'>I do not have an account</h2>
+      <SignUpContainer>
+        <SignUpTitle>I do not have an account</SignUpTitle>
         <span>Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
           <FormInput 
@@ -101,7 +103,7 @@ class SignUp extends React.Component {
           />          
           <CustomButton type='submit'>Sign Up</CustomButton>
         </form>
-      </div>
+      </SignUpContainer>
     )
   }
 
