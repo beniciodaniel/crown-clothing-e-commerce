bdc26cea5c7049c07040661d8a0209e378599622
Benicio <benicio.daniel@gmail.com>
2020-05-16 21:26:29 -0300

    Added Redux Saga. Saga part 1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,22 @@
 import { createStore, applyMiddleware } from 'redux'; //middleware pegar acao antes de ir pro root reducer
 import logger from 'redux-logger'; //para debuggar
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas'
 
 import rootReducer from './root-reducer';
 
-import { persistStore } from 'redux-persist';
-
-import thunk from 'redux-thunk';
-
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));  //criando o mega Store com todos os states
+
+sagaMiddleware.run(fetchCollectionsStart)
 
 const persistor = persistStore(store);
 
